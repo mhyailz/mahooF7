92ff695b4dca6828e9997235c870e3efe734161c
mahoo <mhyailz@gmail.com>
2015-11-07 00:33:27 +0800

    create index.html delete start-page

--- app/src/main/java/me/mahoo/mahoo/MainActivity.java
+++ app/src/main/java/me/mahoo/mahoo/MainActivity.java
@@ -23,11 +23,9 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webView = (WebView) findViewById(R.id.webView);
-
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -35,42 +33,28 @@ public class MainActivity extends Activity {
                 return true;
             }
         });
-
         WebSettings webSettings = webView.getSettings();
-
         webSettings.setJavaScriptEnabled(true);
-
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
                 return super.onJsAlert(view, url, message, result);
             }
         });
-
-
         ConnectivityManager connectMgr = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
-
         NetworkInfo info = connectMgr.getActiveNetworkInfo();
-
         if (info == null) {
             Toast.makeText(getApplicationContext(), "请检查网络，您的手机网络不太通畅喔~", Toast.LENGTH_SHORT).show();
         }
-
         webView.addJavascriptInterface(new JsObject(this), "app");
-
-
-        webView.loadUrl("file:///android_asset/start-page.html");
-
-
+        webView.loadUrl("file:///android_asset/index.html");
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
             Log.i("back", webView.getUrl());
-
             String currentUrl = webView.getUrl();
-
             if (currentUrl.startsWith("file://")) {
                 webView.loadUrl("javascript:appGoBack()");
             } else {
